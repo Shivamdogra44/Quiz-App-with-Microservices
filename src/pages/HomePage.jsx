a33ca7ce87880eef93dfbc85a525403b26a65ac2
Shivamdogra44 <79395872+Shivamdogra44@users.noreply.github.com>
2026-03-01 17:13:33 -0700

    Add HomePage component to display available quizzes

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
-const HomePage = ({ quizzes }) => {
+const HomePage = () => {
+    const quizzes = [
+        { id: 1, title: 'Quiz 1' },
+        { id: 2, title: 'Quiz 2' },
+        { id: 3, title: 'Quiz 3' }
+    ];
+
     return (
         <div>
             <h1>Available Quizzes</h1>
             <ul>
-                {quizzes.map((quiz) => (
-                    <li key={quiz.id}>
-                        <Link to={`/quiz/${quiz.id}`}>{quiz.title}</Link>
-                    </li>
+                {quizzes.map(quiz => (
+                    <li key={quiz.id}>{quiz.title}</li>
                 ))}
             </ul>
         </div>
