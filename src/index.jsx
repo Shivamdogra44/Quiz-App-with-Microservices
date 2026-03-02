bd83fa6f6480fa6b5b57126ca5b1fb58acd21afc
Shivamdogra44 <79395872+Shivamdogra44@users.noreply.github.com>
2026-03-01 17:13:21 -0700

    Create entry point file src/index.jsx

---
+++ src/index.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Entry point for the React application
+ReactDOM.render(<div>Hello, World!</div>, document.getElementById('root'));
